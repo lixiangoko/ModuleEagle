e7794b9bf7e4f00d8cc0ab5900d36d7de3379526
lixiang <2441294779@qq.com>
2017-09-19 09:09:35 +0800

    erro

--- ModuleEagle/app/src/main/java/song/com/cn/myapplication/activity/BusinessActivity.java
+++ ModuleEagle/app/src/main/java/song/com/cn/myapplication/activity/BusinessActivity.java
@@ -2,10 +2,6 @@ package song.com.cn.myapplication.activity;
 
 import android.os.Bundle;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-
 import song.com.cn.myapplication.R;
 
 /**
@@ -24,17 +20,17 @@ public class BusinessActivity extends BaseActivity {
     public void initData() {
 
         // 对图像进行base64编码
-        String img_base64 = "";
-        try {
-            File file = new File(img_file);
-            byte[] content = new byte[(int) file.length()];
-            FileInputStream finputstream = new FileInputStream(file);
-            finputstream.read(content);
-            finputstream.close();
-            img_base64 = (new BASE64Encoder()).encode(content);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
+//        String img_base64 = "";
+//        try {
+//            File file = new File(img_file);
+//            byte[] content = new byte[(int) file.length()];
+//            FileInputStream finputstream = new FileInputStream(file);
+//            finputstream.read(content);
+//            finputstream.close();
+//            img_base64 = (new BASE64Encoder()).encode(content);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return;
+//        }
     }
 }
