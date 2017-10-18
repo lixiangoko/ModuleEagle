be1002d1e5dc234124cd9f76ce6025475593b98e
lixiang <2441294779@qq.com>
2017-10-18 17:55:35 +0800

    mpchartlib

---
+++ app/src/main/java/song/com/cn/myapplication/view/MyCustomXAxisValueFormatter.java
@@ -0,0 +1,37 @@
+package song.com.cn.myapplication.view;
+
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+
+import java.text.DecimalFormat;
+
+/**
+ * Created by Philipp Jahoda on 14/09/15.
+ */
+public class MyCustomXAxisValueFormatter implements IAxisValueFormatter {
+
+    private DecimalFormat mFormat;
+    private ViewPortHandler mViewPortHandler;
+
+    public MyCustomXAxisValueFormatter(ViewPortHandler viewPortHandler) {
+        mViewPortHandler = viewPortHandler;
+        // maybe do something here or provide parameters in constructor
+        mFormat = new DecimalFormat("###,###,###,##0.0");
+    }
+
+    @Override
+    public String getFormattedValue(float value, AxisBase axis) {
+
+        float xScale = mViewPortHandler.getScaleX();
+        if (xScale > 5) {
+            return "4";
+        } else if (xScale > 3) {
+            return "3";
+        } else if (xScale > 1) {
+            return "2";
+        } else {
+            return mFormat.format(value);
+        }
+    }
+}
